bf367c0ae1d14f0a9fc2bf08956ec77d6cba273d
Zek Kelley <6683133+zekkelley@users.noreply.github.com>
2019-12-20 19:08:41 +0800

    added the coalminermugs.com website for testing working with git

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+var CACHE_NAME = "first";
+var CACHED_URLS = [
+  '/assets/logo5.png',
+  '/assets/logo5.webp',
+  '/assets/DO_Proudly_Hosted_Badge_Grey-1652c782.png',
+  '/assets/DO_Proudly_Hosted_Badge_Grey-1652c782.webp',
+  '/assets/favicon.ico',
+  '/index-offline.html',
+  '/assets/css/bootstrap.min.css',
+  '/index.css'
+];
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(CACHED_URLS);
+    })
+  );
+});
+
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request, {ignoreSearch: true}).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          return caches.match("/index-offline.html");
+        }
+      });
+    })
+  );
+});
